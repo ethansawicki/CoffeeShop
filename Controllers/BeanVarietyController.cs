2cb57496ee1ce36d766f3d810e46b9f2c4f09e4b
Ethan Sawicki <ethansawicki2797@gmail.com>
2023-03-06 21:40:18 -0600

    Add project files.

---
+++ Controllers/BeanVarietyController.cs
@@ -0,0 +1,55 @@
+﻿using CoffeeShop.Models;
+using CoffeeShop.Repositories;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BeanVarietyController : ControllerBase
+    {
+        private readonly IBeanVarietyRepository _beanVarietyRepository;
+        public BeanVarietyController(IBeanVarietyRepository beanVarietyRepository)
+        {
+            _beanVarietyRepository = beanVarietyRepository;
+        }
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_beanVarietyRepository.GetAll());
+        }
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var variety = _beanVarietyRepository.Get(id);
+            if (variety == null)
+            {
+                return NotFound();
+            }
+            return Ok(variety);
+        }
+        [HttpPost]
+        public IActionResult Post(BeanVariety beanVariety)
+        {
+            _beanVarietyRepository.Add(beanVariety);
+            return CreatedAtAction("Get", new { id = beanVariety.Id }, beanVariety);
+        }
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, BeanVariety beanVariety)
+        {
+            if (id != beanVariety.Id)
+            {
+                return BadRequest();
+            }
+            _beanVarietyRepository.Update(beanVariety);
+            return NoContent();
+        }
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _beanVarietyRepository.Delete(id);
+            return NoContent();
+        }
+    }
+}
